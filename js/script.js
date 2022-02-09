f2b1e2d93a2afa1fc3cb4c6b6b217de9de0539c7
Joli Holmes <holmesjoli@gmail.com>
2022-02-09 15:55:22 -0500

    extract value of selected country

--- js/script.js
+++ js/script.js
@@ -19,11 +19,17 @@ d3.csv("./data/cit_long.csv").then(function(data) {
     let text = "";
 
     for (let i = 0; i < allCountry.length; i++) {
-        console.log(allCountry[i]);
-        text += `<input type="radio" id="cntry-input-${allISO3[i]}" name="cntry" value="cntry">
+
+        if (i === 0) {
+            text += `<input type="radio" id="cntry-input-${allISO3[i]}" class="country--option" name="cntry" value="${allISO3[i]}" checked>
         <label for="cntry-input-${allISO3[i]}">${allCountry[i]}</label><br>`;
+        } else {
+            text += `<input type="radio" id="cntry-input-${allISO3[i]}" class="country--option" name="cntry" value="${allISO3[i]}">
+        <label for="cntry-input-${allISO3[i]}">${allCountry[i]}</label><br>`;
+        }
     }
 
     document.getElementById("cntry-input").innerHTML = text;
 
+    let selectedCntry = document.querySelector('input[name="cntry"]:checked').value;
 });
