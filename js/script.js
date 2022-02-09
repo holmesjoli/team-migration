6078d6c4b235f78891d4a9e304bb85ff5657dc94
Joli Holmes <holmesjoli@gmail.com>
2022-02-09 15:12:59 -0500

    created a function so it is easy to return a unique value from a data frame

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+function uniqueArray(data, variable) {
+
+    let all = data.map(function(d) {
+        return d[variable];
+    })
+
+    return [...new Set(all)];
+}
+
+
 d3.csv("./data/cit_long.csv").then(function(data) {
 
     console.log(data);
 
-    let allCountry = data.map(function(d) {
-        return d.country;
-    })
-
-    const uniqueCountry = [...new Set(allCountry)];
+    let allCountry = uniqueArray(data, "country");
+    let allISO3 = uniqueArray(data, "iso3");
 
 });
