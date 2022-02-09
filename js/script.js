fa438989491e28f4d2dec646eb9becb4397b1f9d
Joli Holmes <holmesjoli@gmail.com>
2022-02-09 15:10:29 -0500

    extracted unique country names

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-d3.csv("./data/cit_long.csv", parse).then(function(data) {
+d3.csv("./data/cit_long.csv").then(function(data) {
+
+    console.log(data);
+
+    let allCountry = data.map(function(d) {
+        return d.country;
+    })
+
+    const uniqueCountry = [...new Set(allCountry)];
 
 });
