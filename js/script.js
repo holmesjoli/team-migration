e1fe1812892bd7a6b16c378d7d7b3b5c68a04114
Joli Holmes <holmesjoli@gmail.com>
2022-02-18 12:48:48 -0500

    moved function to helper functions file to keep everything modular

--- js/script.js
+++ js/script.js
@@ -27,19 +27,7 @@ d3.csv("./data/cit_long.csv").then(function(citLong) {
         document.getElementById("region-input").innerHTML = text;
 
         //Dropdown Menu
-        $('.dropdown').click(function () {
-            $(this).attr('tabindex', 1).focus();
-            $(this).toggleClass('active');
-            $(this).find('.dropdown-menu').slideToggle(300);
-        });
-        $('.dropdown').focusout(function () {
-            $(this).removeClass('active');
-            $(this).find('.dropdown-menu').slideUp(300);
-        });
-        $('.dropdown .dropdown-menu li').click(function () {
-            $(this).parents('.dropdown').find('span').text($(this).text());
-            $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
-        });
+        dropdown()
 
         //End Dropdown Menu
 
