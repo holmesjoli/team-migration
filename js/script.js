e946b2cb6b77c8876d50155f4a4fa854578720fb
Joli Holmes <holmesjoli@gmail.com>
2022-02-19 13:32:23 -0500

    made the circles update according to the selection

--- js/script.js
+++ js/script.js
@@ -21,6 +21,50 @@ d3.csv("./data/cit_long.csv").then(function(citLong) {
 
         dropdown()
 
+        let dataInit = xwalkRegion.filter(function(d) {
+            return d.subregion_code === "913";
+        });
+
+        let initCntries = uniqueArray(dataInit, "region");
+
+        const dim = generateMatrix(initCntries.length);
+        const nCol = dim.nCol;
+        const nRow = dim.nRow;
+
+        const xPos = xPosition(nCol, nRow);
+        const yPos = yPosition(nCol, nRow);
+
+        dataInit.forEach(function(d, i) {
+            d.x = xPos[i];
+            d.y = yPos[i];
+        });
+
+        const width = 1200;
+        const height = 300;
+        const margin = {top: 50, left: 100, right: 100, bottom: 125};
+
+        const svg = d3.select("#countries")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        const xScale = d3.scaleLinear()
+            .domain([1, nCol])
+            .range([margin.left, width-margin.right]);
+
+        const yScale = d3.scaleLinear()
+            .domain([1, nRow])
+            .range([height-margin.bottom, margin.top]);
+
+        svg.selectAll("circle")
+            .data(dataInit, function(d) { return d.iso3; })
+            .enter()
+            .append("circle")
+                .attr("cx", function(d) { return xScale(d.x); })
+                .attr("cy", function(d) { return yScale(d.y); })
+                .attr("r", 10)
+                .attr("fill", "black");
+
         // Filter the data according to the users input
         d3.selectAll(".dropdown-menu li").on("click", function() {
 
@@ -30,24 +74,67 @@ d3.csv("./data/cit_long.csv").then(function(citLong) {
                 return d.subregion_code === selectedRegion;
             });
 
+            const selectedCntries = uniqueArray(dataFiltered, "region");
+            const dim = generateMatrix(selectedCntries.length);
+            const nCol = dim.nCol;
+            const nRow = dim.nRow;
+
+            const xPos = xPosition(nCol, nRow);
+            const yPos = yPosition(nCol, nRow);
+            console.log("nRow", nRow);
+            console.log("nCol", nCol);
+
+            dataFiltered.forEach(function(d, i) {
+                d.x = xPos[i];
+                d.y = yPos[i];
+            });
+
+            console.log(dataFiltered);
+
+            xScale.domain([1, nCol]);
+            yScale.domain([1, nRow]);
             dataFiltered.sort(function(x, y){
                 return d3.ascending(x.country, y.country);
             });
 
-            countryMenu(dataFiltered, "#countries");
+            let c = svg.selectAll("circle")
+                .data(dataFiltered, function(d) {return d.iso3;});
 
-            d3.selectAll(".selectRegion").on("click", function() {
+            c
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) { return xScale(d.x); })
+            .attr("cy", function(d) { return yScale(d.y); })
+            .attr("r", 0)
+            .merge(c)
+            .transition()
+            .duration(1000)
+            .delay(1000)
+            .attr("cx", function(d) { return xScale(d.x); })
+            .attr("cy", function(d) { return yScale(d.y); })
+            .attr("r", 10);
 
-                let selectedCntry = d3.select(this).property("id");
+            c.exit()
+            .transition()
+            .duration(1000)
+            .delay(1000)
+            .attr("r", 0)
+            .remove();
 
-                cntryFiltered = dataFiltered.filter(function(d) {
-                    return d.iso3 === selectedCntry;
-                })
+            // countryMenu(dataFiltered, xScale, yScale, svg);
 
-                document.getElementById("selectedCountry").innerHTML = cntryFiltered[0].region;
-                document.getElementById("path-sentence").style["visibility"] = "visible";
+            // d3.selectAll(".selectRegion").on("click", function() {
 
-            });
+            //     let selectedCntry = d3.select(this).property("id");
+
+            //     cntryFiltered = dataFiltered.filter(function(d) {
+            //         return d.iso3 === selectedCntry;
+            //     })
+
+            //     document.getElementById("selectedCountry").innerHTML = cntryFiltered[0].region;
+            //     document.getElementById("path-sentence").style["visibility"] = "visible";
+
+            // });
         });
     });
 });
