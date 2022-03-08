98f8d706ff2a656c131508c9bb5f4bc797f9fe28
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2022-03-08 15:23:13 -0500

    commit

--- svelte-development/public/build/bundle.js
+++ svelte-development/public/build/bundle.js
@@ -8819,6 +8819,7 @@ var app = (function () {
     			div2 = element("div");
     			div0 = element("div");
     			t5 = space();
+<<<<<<< HEAD
     			div1 = element("div");
     			t6 = space();
     			div7 = element("div");
@@ -8853,6 +8854,26 @@ var app = (function () {
     			add_location(div8, file$4, 54, 4, 1351);
     			attr_dev(section, "class", "big-butterfly__container");
     			add_location(section, file$4, 50, 2, 1237);
+=======
+    			div3 = element("div");
+    			add_location(h1, file$4, 50, 4, 1282);
+    			attr_dev(div0, "id", "citizenship-paths");
+    			add_location(div0, file$4, 53, 4, 1352);
+    			attr_dev(div1, "id", "warnings");
+    			attr_dev(div1, "class", "svelte-1mt2l5u");
+    			add_location(div1, file$4, 57, 6, 1462);
+    			attr_dev(div2, "id", "specification");
+    			attr_dev(div2, "class", "svelte-1mt2l5u");
+    			add_location(div2, file$4, 58, 6, 1495);
+    			attr_dev(div3, "id", "definitions");
+    			attr_dev(div3, "class", "svelte-1mt2l5u");
+    			add_location(div3, file$4, 59, 6, 1533);
+    			attr_dev(div4, "id", "documentation");
+    			attr_dev(div4, "class", "svelte-1mt2l5u");
+    			add_location(div4, file$4, 56, 4, 1430);
+    			attr_dev(section, "class", "big-butterfly__container svelte-1mt2l5u");
+    			add_location(section, file$4, 49, 2, 1234);
+>>>>>>> parent of f5acd8c (added bundle)
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, section, anchor);
