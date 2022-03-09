8723871f56029da47f60f85d13855516371de02e
Joli Holmes <holmesjoli@gmail.com>
2022-03-09 12:05:32 -0500

    standardized the question numbering

--- svelte-development/src/helper.js
+++ svelte-development/src/helper.js
@@ -83,19 +83,23 @@ export function createUnnecessaryQuestions(allQuestions, possibleQuestions) {
 
 export class clickContainer {
 
+  pathTrue = "#000000";
+  pathFalse = "#FFFFFF";
+  pathNA = "#";
+
   constructor() {
-    this.Q3 = false;
-    this.Q2 = false;
-    this.Q1 = false;
+    this.Q03 = false;
+    this.Q02 = false;
+    this.Q01 = false;
     this.Q22 = false;
     this.Q20 = false;
     this.Q14 = false;
     this.Q17 = false;
-    this.Q7 = false;
+    this.Q07 = false;
     this.Q08 = false;
     this.Q29 = false;
     this.Q21 = false;
-    this.Q9 = false;
+    this.Q09 = false;
     this.Q10 = false;
     this.Q11 = false;
     this.Q12 = false;
@@ -107,15 +111,185 @@ export class clickContainer {
     this.Q27 = false;
     this.Q28 = false;
     this.Q15 = false;
-    this.Q4 = false;
-    this.Q5 = false;
-    this.Q18 =false;
-    this.Q6 = false;
+    this.Q04 = false;
+    this.Q05 = false;
+    this.Q18 = false;
+    this.Q06 = false;
     this.Q19 = false
     this.Q16 = false;
+
+    this.A01a = false;
+    this.A01b = false;
+    this.A02a = false;
+    this.A02b = false;
+    this.A06a = false;
+    this.A06b = false;
+    this.A06c = false;
+    this.A06d = false;
+    this.A06e = false;
+    this.A06f = false;
+    this.A07 = false;
+    this.A08 = false;
+    this.A09 = false;
+    this.A10 = false;
+    this.A11 = false;
+    this.A12a = false;
+    this.A12b = false;
+    this.A13 = false;
+    this.A14 = false;
+    this.A16 = false;
+    this.A18 = false;
+    this.A19 = false;
+    this.A21 = false;
+    this.A22 = false;
+    this.A23 = false;
+    this.A24 = false;
+    this.A25 = false;
+    this.A26 = false;
   }
 
   updateClick(id) {
     this[id] = true;
+  }
+
+  highlightPaths() {
+    //A01a
+    if (this.Q03 && this.Q02) {
+      this.A01a = true;
+    }
+
+    //A01b
+    if (this.Q03 && !this.Q02) {
+      this.A01b = true;
+    }
+
+    //A02a
+    if (this.Q02) {
+      this.A02a = true;
+    }
+
+    //A02b
+    if (this.Q01 & this.Q02) {
+      this.A02b = true;
+    }
+
+    //A06a //come back to requires extra coding
+    // if (this.Q22 & blah) {
+    //   this.A06a = true;
+    // }
+
+    //A06b
+    if (this.Q22 && this.Q24) {
+      this.A06b = true;
+    }
+
+    //A06c
+    if (this.Q22 && this.Q25) {
+      this.A06c = true;
+    }
+
+    //A06d
+    if (this.Q22 && this.Q25) {
+      this.A06d = true;
+    }
+    //A06e
+    if (this.Q22 && this.Q27) {
+      this.A06e = true;
+    }
+
+    //A06f
+    if (this.Q22 && this.Q28) {
+      this.A06f = true;
+    }
+
+    //A07
+    if (this.Q20) {
+      this.A07 = true;
+    }
+
+    //A08
+    if (this.Q14 && this.Q15) {
+      this.A08 = true;
+    }
+
+    //A09
+    if (this.Q23 && this.Q04 && this.Q06) {
+      this.A09 = true;
+    }
+
+    //A10
+    if (this.Q03 && this.Q05) {
+      this.A10 = true;
+    }
+
+    //A11
+    if (this.Q17 && this.Q18) {
+      this.A11 = true;
+    }
+
+    //A12a
+    if (this.Q17 && this.Q18 && this.Q19) {
+      this.A12b = true;
+    }
+
+    //A12b
+    if (this.Q17 && this.Q18 && !this.Q19) {
+      this.A12a = true;
+    }
+
+    //A13
+    if (this.Q14 && this.Q15 && this.Q16) {
+      this.A13 = true;
+    }
+
+    //A14
+    if (this.Q03 && this.Q04 && this.Q06) {
+      this.A14 = true;
+    }
+
+    //A16
+    if (this.Q07) {
+      this.A16 = true;
+    }
+
+    //A18
+    if (this.Q08) {
+      this.A18 = true;
+    }
+
+    //A19
+    if (this.Q29) {
+      this.A19 = true;
+    }
+
+    //A21
+    if (this.Q21) {
+      this.A21 = true;
+    }
+
+    //A22
+    if (this.Q09) {
+      this.A22 = true;
+    }
+
+    //A23
+    if (this.Q10) {
+      this.A23 = true;
+    }
+
+    //A24
+    if (this.Q11) {
+      this.A24 = true;
+    }
+
+    //A25
+    if (this.Q12) {
+      this.A25 = true;
+    }
+
+    //A26
+    if (this.Q13) {
+      this.A26= true;
+    }
   }
 }
